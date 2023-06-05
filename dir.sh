83602f4846ff6001816818ad64f418d8eacafc37
jan <ivan.stseoaniuk@gmail.com>
2023-06-05 21:32:32 +0200

    added docker-image startup .sh script

--- dir.sh
+++ dir.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+docker run --cap-add=IPC_LOCK -e 'VAULT_LOCAL_CONFIG={"storage": {"file": {"path": "/vault/file"}}, "listener": [{"tcp": { "address": "0.0.0.0:8200", "tls_disable": true}}], "default_lease_ttl": "168h", "max_lease_ttl": "720h", "ui": true}' -p 8200:8200 vault server
+
